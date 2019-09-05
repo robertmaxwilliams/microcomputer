bb759bac63110ff11aa7a0db18811e0abd34ca32
robertmaxwilliams <robertmaxwilliams@gmail.com>
2019-09-05 17:37:24 -0400

    added assembly comments

--- project1/calc.s
+++ project1/calc.s
@@ -1,83 +1,89 @@
 //Assembly Subroutine
-//Eugene Rockey, Copyright 2018, All Rights Reserved
+//original: Eugene Rockey, Copyright 2018, All Rights Reserved
+//modified: Robert Williams, Kopyleft, All Rights Reversed
 
-.section ".text" // not sure what this does
+.section ".text" // declare read only
 
-.global next_char
-next_char:
-    ADD r0,#1 //How is the ARM calling convention obeyed here?
-    MOV pc,lr
+// in all of these, arguments come in in r0 and r1, and result 
+// is put in r0 before using "MOV pc, lr".
+// MOV pc, lr means to set the program counter to the link register, 
+// where the link registers is the place in the program that called
+// out function.
 
 .global add
 add:
-    ADD r0, r1
+    ADD r0, r1 // trivial call to add instruction
+	MOV pc, lr // return
 
 .global sub
     sub:
-	SUB r0, r1
-	MOV pc, lr
-    MOV pc, lr
+	SUB r0, r1 // trivial call to subtract instruction
+    MOV pc, lr // return
 
 .global mult
 mult:
-    MUL r0, r0, r1
-    MOV pc, lr
+    MUL r0, r0, r1 // trivial call to multiply instruction
+    MOV pc, lr     // return
 
 .global divide
 divide:
-    // make r0 and r1 positive, and increment r3 for each one 
-    // that had its sign flipped
-    MOV r3, #0
-    cmp r0, #0
-    NEGLT r0, r0
+
+    MOV r3, #0   // r3 = 0
+
+    // if r0 is negative, make it positive and add one to r3
+    cmp r0, #0   // if r0 < 0:
+    NEGLT r0, r0 //     r0 = -r0
+    ADDLT r3, #1 //     r3 += 1
+
+    // if r1 is negative, make it positive and add one to r3
+    cmp r1, #0   // if r1 < 0
+    NEGLT r1, r1 //     r1 = -r1
     ADDLT r3, #1
 
-    cmp r1, #0
-    NEGLT r1, r1
-    ADDLT r3, #1
+    MOV r2, #0 // temp to store result
 
-    MOV r2, #0 // r0 divided by r1, r2 is counter
-loop1:
+loop1: // start of division loop
     SUBS r0, r0, r1 // r0 = r0 - r2
-    ADD r2, r2, #1 // r2 = r2 + 1
+    ADD r2, r2, #1 // r2 = r2 + 1, count number of times we have to subtract
     BGE loop1 // branch if r1 is greater than r0
-    MOV r0, r2
+    MOV r0, r2 // move answer into return register
     SUB r0, #1
 
 
     // negate r0 if r3's lsb is 1
-    AND r3, r3, #1
-    CMP r3, #1
-    NEGEQ r0, r0
+    AND r3, r3, #1 // get lsb of r3
+    CMP r3, #1     // if r3 == 1:
+    NEGEQ r0, r0   //     r0 = -r0
 
-    MOV pc, lr
+    MOV pc, lr // return
 
 
 .global rem
 rem:
-    // make r0 and r1 positive, and increment r3 for each one 
-    // that had its sign flipped
-    MOV r3, #0
-    cmp r0, #0
-    NEGLT r0, r0
-    ADDLT r3, #1
+    MOV r3, #0  // r3 = 0
 
-    cmp r1, #0
-    NEGLT r1, r1
-    ADDLT r3, #2
+    // make r0 and r1 positive, and 
+    // have r3 = 1 * is_r0_pos + 2 * is_r1_pos
+    // so we can tell what case we started with when we get to the end.
+    cmp r0, #0    // if r0 < 0:
+    NEGLT r0, r0  //     r0 = -r0
+    ADDLT r3, #1  //     r3 += 1
+
+    cmp r1, #0    // if r1 < 0:
+    NEGLT r1, r1  //     r1 = -r1
+    ADDLT r3, #2  //     r3 += 2
 
     MOV r2, #0 // r0 divided by r1, r2 is counter
 loop2:
     SUBS r0, r0, r1 // r0 = r0 - r2
     ADD r2, r2, #1 // r2 = r2 + 1
     BGE loop2 // branch if r1 is greater than r0
-    ADD r0, r0, r1 // undo one step
+    ADD r0, r0, r1 // undo one step, since we always go one too far.
 
+    // negate r0 if r3 = 1, aka numerator is negative and denominator is positive
+    CMP r3, #1   // if r3 == 1:
+    NEGEQ r0, r0 //     r0 = -r0
 
-    // negate r0 if r3 = 1
-    CMP r3, #1
-    NEGEQ r0, r0
+    MOV pc, lr // return
 
-    MOV pc, lr
-
-.end // end of whole file ;)
+.end // end of whole file
